1b544e4ca61091f467f78181bf27a074af7e54e7
melek <malekgomri881@hotmail.com>
2024-11-14 21:29:08 +0100

    fix error urls.py

--- project_management/urls.py
+++ project_management/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('utilisateurs/', include('utilisateurs.urls')),  
-    path('', include('utilisateurs.urls')),  
+    path('utilisateurs/', include('utilisateurs.urls')), 
     path('projets/', include('projets.urls')),
 ]
